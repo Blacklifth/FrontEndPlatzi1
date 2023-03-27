43ebf18dff455703688c5c46802ef2ec07eedac7
Blacklifth <marcelo.blc.syb@gmail.com>
2023-03-27 11:48:39 -0300

    agregado product shopping cart list. añadidas funciones in main js para desplegar estos menu. Añadidos los estilos a Stylescss para my order content

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 /*añade selector de clase en una variable*/
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-/*añade un evento de tipo "clic" a menu email que esta apuntando al elemento con la clase navbar-email*/
-menuEmail.addEventListener("click", toggleDesktopMenu);
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
-}
-
 const menuMobileIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuNavShoppingCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
+/*añade un evento de tipo "click" a la variable que esta apuntando al elemento con la clase del selector*/
+menuEmail.addEventListener("click", toggleDesktopMenu);
 menuMobileIcon.addEventListener("click", toggleMobileMenu);
+menuNavShoppingCart.addEventListener("click", toggleCarritoAside);
+
 /*esta funcion nos permite quitar u otorgar la clase inactive en el elemento seleccionado*/
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+    //si el aside esta abierto
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+    desktopMenu.classList.toggle("inactive");
+}
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+    //si el aside esta abierto
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+    //si el menu esta abierto
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
 }
